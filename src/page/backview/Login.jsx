7783aac207c398fd62f29ae0309c29415f424e45
M. Ridwan Zalbina <ridwanzal@fairtech.com.sg>
2022-10-10 17:43:34 +0700

    Update login page
    - Update login page

---
+++ src/page/backview/Login.jsx
@@ -0,0 +1,33 @@
+import '../../styles/styles.scss';
+import { Image, Shimmer } from 'react-shimmer';
+import Footer from '../../components/Footer';
+
+function Designs() { 
+    return ( 
+        <>
+        <div className="section-general">
+            <div className="wrapper">
+                <div className="wrapper-inner">
+                    <div className="wrapper-content">
+                        <h3>Login</h3>
+                        <div className='form-g'>
+                            <input className='form-in' placeholder='Email/username'></input>
+                        </div>
+                        <br/>
+                        <div className='form-g'>
+                            <input className='form-in' placeholder='Password'></input>
+                        </div>
+                        <br/>
+                        <button type="submit" className='mybtn'>
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Footer></Footer>
+        </>
+    );
+}
+
+export default Designs;
