85c567193f422cd8e649155c69b859ded6562315
M. Ridwan Zalbina <ridwanzal@fairtech.com.sg>
2022-10-18 14:48:33 +0700

    Update new lib for blog submission

--- src/page/backview/BlogSubmit.jsx
+++ src/page/backview/BlogSubmit.jsx
@@ -1,13 +1,19 @@
 import '../../styles/styles.scss';
 import Footer from '../../components/Footer';
 import { useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
 
 function BlogSubmit() { 
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
 
-    function handleSubmit(){
+    function handleSubmit(event){
+        event.preventDefault();
+        let titleData = title;
+        let contentData = content;
 
+        console.log(titleData)
+        console.log(contentData);
     }
     
 
@@ -19,12 +25,16 @@ function BlogSubmit() {
                     <form onSubmit={handleSubmit} className="wrapper-content">
                     <div className='form-g'>
                         <label className='display-block'>Title:</label>
-                        <input className='form-in' type="text" name="" value=""/>
+                        <input className='form-in' type="text" name="" value={title}  onChange={(e) => setTitle(e.target.value)}/>
                     </div>
 
                     <div className='form-g'>
                         <label className='display-block'>Content:</label>
-                        <textarea className='form-in' type="text" name="" rows="10"></textarea>
+                        <MDEditor
+                            value={content}
+                            onChange={setContent}
+                        />
+                        <MDEditor.Markdown source={content} style={{ whiteSpace: 'pre-wrap' }} />
                     </div>
 
                     <input type="submit" className='btn default outline light' value="Submit"/>
