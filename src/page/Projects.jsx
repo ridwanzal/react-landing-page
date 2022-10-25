ec900e1da915905ed1b07e020c7fb16f8eb0d375
M. Ridwan Zalbina <ridwanzal@fairtech.com.sg>
2022-10-25 12:43:55 +0700

    Update react landing page
    - Fix some ui issues
    - Blog post update

--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -5,21 +5,95 @@ function Projects() {
     return ( 
         <>
         <div className="section-general">
-            <div className="wrapper">
-                <div className="wrapper-inner">
-                    <div className="wrapper-content">
-                        <div className='center'>
-                            <h3>Page still in progress</h3>
-                            <p className='incoming'>
-                                Coming soon
-                            </p>
-                            <p>Thank you so much for visiting this page. We still working on it. Stay tune.</p>
+                <div className="wrapper">
+                    <div className="wrapper-inner">
+                        <div className="wrapper-content wrapper-content--contain">
+                            <div className='content-small contain-wrap half'>
+                                <h3>Projects from 2021</h3>
+                                <ul>
+                                    <li>
+                                        MRP Core system PT KAI Balai Yasa Lahat (confidential)
+                                    </li>
+                                    <li>
+                                        CBT (Computer Based Test) Mahad Aly An-Nuaimy Jakarta Selatan 
+                                    </li>
+                                    <li>
+                                        Ezkop product design for web and mobile app 
+                                    </li>
+                                    <li>
+                                        Ezkop Web & Mobile Platform 
+                                    </li>
+                                    <li>
+                                        Virtual Expo platform for m2 <a href="https://m2.imeet.id/" target="_blank">link</a>
+                                    </li>
+                                    <li>
+                                        Resiku - Cek resi dan ongkir (Mobile web app platform)
+                                    </li>
+                                    <li>
+                                        Virtual Exhibition platform for m2 <a href="https://m2.imeet.id/" target="_blank">link</a>
+                                    </li>
+                                    <li>
+                                        SIKAV - Sistem Kalkulator Inovasi RISTEK BRIN (dead)
+                                    </li>
+                                    <li>
+                                        SITAMPAN - All in one security web tools (dead)
+                                    </li>
+                                    <li>
+                                        Dashboard UNSRI (dead)
+                                    </li>
+                                    <li>
+                                        Satu data UNSRI API Gateway (dead)
+                                    </li>
+                                </ul>
+
+                                <h3>Projects from 2020</h3>
+                                <ul>
+                                    <li>
+                                        Universitas Binawan (dead)
+                                    </li>
+                                    <li>
+                                        Sistem Pencatatan Cuti (confidential)
+                                    </li>
+                                    <li>
+                                        BPU Unsri (collaboration with AdityaDees) <a href="https://bpu.unsri.ac.id/" target="_blank">link</a>
+                                    </li>
+                                    <li>
+                                        Maximus Hotel Palembang (Reservation system)
+                                    </li>
+                                    <li>
+                                        Marco Digital Partner Landing page website <a target="_blank" href="https://www.marcodigitalpartner.com/">link</a>
+                                    </li>
+                                    <li>
+                                        Alisha Marsya <a target="_blank" href="https://alishamarsya.com/">link</a>
+                                    </li>
+                                    <li>
+                                        Scafol Mobile App (dead)
+                                    </li>
+                                </ul>
+                                <h3>Projects from 2019-2020</h3>
+                                <ul>
+                                    <li>
+                                        Rumah Sakit Ar-Rasyid Palembang (not maintain anymore) <a target="_blank" href="https://rsarrasyid.id/"></a>
+                                    </li>
+                                    <li>
+                                        Web PUBM Sumsel (dead)
+                                    </li>
+                                    <li>
+                                        IDD Web - TIS Network (dead)
+                                    </li>
+                                    <li>
+                                        Sistem pendukung simulasi pembelian rumah berbasis syariah (confidential)
+                                    </li>
+                                    <li>
+                                        Sistem deteksi dini pencegahan phising (phising-live-detector)
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Footer></Footer>
+            <Footer></Footer>
         </>
     );
 }
